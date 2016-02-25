e1e100c32e89e805ab2c574d12023f078260ccd1
tallkeith <tallestkeith@gmail.com>
2016-02-25 15:01:47 -0500

    first commit

---
+++ t3.rb
@@ -0,0 +1,19 @@
+require "pry"
+
+# DATA IN THE SYSTEM
+# * board
+# * player1 (name/piece), player2
+# * win conditions
+# * current_player
+
+
+board = [8,1,6,3,5,7,4,9,2]
+
+def show_board(board)
+	3.times do |row|
+		puts board[row * 3, 3].join(" | ")
+	end
+end
+
+show_board(board)
+
