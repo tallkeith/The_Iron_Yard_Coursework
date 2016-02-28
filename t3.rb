22eee7ef4c8e826f0dbceb8b26ac060405c57d58
tallkeith <tallestkeith@gmail.com>
2016-02-28 14:09:31 -0500

    I have no idea what I'm doing

--- t3.rb
+++ t3.rb
@@ -5,13 +5,14 @@ require "pry"
 # * player1 (name/piece), player2
 # * win conditions
 # * current_player
+# * player score
 
-
+# * selection
 # * start game
 
-
-
+MAX_TURNS = 9
 board = [8,1,6,3,5,7,4,9,2]
+
 
 def show_board(board)
 	3.times do |row|
@@ -19,5 +20,53 @@ def show_board(board)
 	end
 end
 
-show_board(board)
+def win?(player_score, comp_score)
+
+	if player_score.reduce(:+) == 15
+		puts "Congratulations, you win!"
+	elsif comp_score.reduce(:+) == 15
+		puts "Sorry, you lost"
+	elsif turn_count == MAX_TURNS
+		puts "You tied!"
+	else
+		next_turn
+	end
+end
+
+def score(player_score)
+	
+	puts "Please select a square 1-9"
+	choice = gets.chomp.to_i
+	until (1..9).include?(choice) && !player_score.include?(choice)
+		if player_score.include?(choice)
+			puts "That spot is already taken"
+		end
+		puts "Selection must be a number 1-9"
+	end
+	
+	player_score.push(choice)
+	choice
+end
+
+def icons
+	player_piece = "X"
+	comp_piece = "O"
+	# score(player_score) = spot
+	spot = board.index(input)
+	board[spot] = player_piece
+
+	show_board
+
+end
+
+def play_game(board)
+	
+	player_score = []
+	comp_score = []
+	show_board(board)
+	move = score(player_score)
+	icons
+end
+
+play_game(board)
 
